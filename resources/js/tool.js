f174d54f7da674600ee5fed20996365e913c63f2
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-12-15 22:20:22 +0330

    Some Refactoring

--- resources/js/tool.js
+++
@@ -1,9 +0,0 @@
-Nova.booting((Vue, router, store) => {
-  router.addRoutes([
-    {
-      name: 'shaghool',
-      path: '/shaghool',
-      component: require('./components/Tool'),
-    },
-  ])
-})
